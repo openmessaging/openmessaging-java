e82c7bb1f663670c552762491b472fc80adfd2a2
xinyuzhou.zxy <xinyuzhou.zxy@alibaba-inc.com>
2017-03-13 17:54:04 +0800

    Add javadoc for MessageFactory and ServiceLifecycle

--- code/messaging-user-level-api/java/src/main/java/io/openmessaging/ServiceLifecycle.java
+++ code/messaging-user-level-api/java/src/main/java/io/openmessaging/ServiceLifecycle.java
@@ -17,11 +17,33 @@
 
 package io.openmessaging;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
+ * The {@code ServiceLifecycle} defines a lifecycle interface for a OMS related service endpoint, like {@link Producer},
+ * {@link PushConsumer}, and so on.
+ * <p/>
+ * If the service endpoint class implements the {@code ServiceLifecycle} interface, most of the containers can manage
+ * the lifecycle of the corresponding service endpoint objects easily.
+ * <p/>
+ * Any service endpoint should support repeated restart if it implements the {@code ServiceLifecycle} interface.
+ *
  * @author vintagewang@apache.org
+ * @author yukon@apache.org
  */
 public interface ServiceLifecycle {
+    /**
+     * Used for start or initialization of a service endpoint. A service endpoint instance will be in a ready state
+     * after this method has been completed.
+     */
+    @PostConstruct
     void start();
 
+    /**
+     * Notify a service instance of the end of its life cycle. Once this method completes, the service endpoint could be
+     * destroyed and eligible for garbage collection.
+     */
+    @PreDestroy
     void shutdown();
 }
