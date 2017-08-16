603ffc9a6648b6363b0018d5b9d3e1582baf21e9
vintagewang <vintage.wang@apache.org>
2017-08-16 13:51:07 +0800

    refactor ProducerInterceptor & PushConsumerInterceptor

--- code/messaging-user-level-api/java/src/main/java/io/openmessaging/interceptor/ProducerInterceptor.java
+++ code/messaging-user-level-api/java/src/main/java/io/openmessaging/interceptor/ProducerInterceptor.java
@@ -1,11 +1,16 @@
 package io.openmessaging.interceptor;
 
+import io.openmessaging.KeyValue;
+import io.openmessaging.Producer;
+
 /**
  * @author vintagewang@apache.org
  * @version OMS 1.0
  * @since OMS 1.0
  */
 public interface ProducerInterceptor {
+    Producer constructProducer(KeyValue properties);
+
     void sendBefore(SendBeforeContext context);
 
     void sendAfter(SendAfterContext context);
--- code/messaging-user-level-api/java/src/main/java/io/openmessaging/interceptor/PushConsumerInterceptor.java
+++ code/messaging-user-level-api/java/src/main/java/io/openmessaging/interceptor/PushConsumerInterceptor.java
@@ -1,11 +1,16 @@
 package io.openmessaging.interceptor;
 
+import io.openmessaging.KeyValue;
+import io.openmessaging.PushConsumer;
+
 /**
  * @author vintagewang@apache.org
  * @version OMS 1.0
  * @since OMS 1.0
  */
 public interface PushConsumerInterceptor {
+    PushConsumer constructPushConsumer(KeyValue properties);
+
     void onMessageBefore(OnMessageBeforeContext context);
 
     void onMessageAfter(OnMessageAfterContext context);
