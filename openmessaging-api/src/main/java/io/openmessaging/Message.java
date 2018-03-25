abe64d331fedc7dab4827d15d1da2a91e6a8eea2
尘央 <xinyuzhou.zxy@alibaba-inc.com>
2018-03-25 20:53:29 +0800

    Polish the javadoc of Message

--- openmessaging-api/src/main/java/io/openmessaging/Message.java
+++ openmessaging-api/src/main/java/io/openmessaging/Message.java
@@ -21,19 +21,32 @@ package io.openmessaging;
  * The {@code Message} interface is the root interface of all OMS messages, and the most commonly used OMS message is
  * {@link BytesMessage}.
  * <p>
- * Most message-oriented middleware (MOM) products treat messages as lightweight entities that consist of a header and a
- * body, like <a href="http://rocketmq.apache.org/">Apache RocketMQ</a>. The header contains fields used for message
- * routing and identification; the body contains the application data being sent.
+ * Most message-oriented middleware (MOM) products treat messages as lightweight entities that consist of
+ * header and body and is used by separate applications to exchange a piece of information,
+ * like <a href="http://rocketmq.apache.org/">Apache RocketMQ</a>.
  * <p>
- * The {@code Message} is a lightweight entity that only contains the property related information of a specific message
- * object, and the {@code Message} is composed of the following parts:
- *
- * <UL>
- * <LI>Header - All messages support the same set of header fields. Header fields contain values used by both
- * clients and providers to identify and route messages.
- * <LI>Properties - Each message contains a built-in facility for supporting application-defined property values.
- * Properties provide an efficient mechanism for supporting application-defined message filtering.
- * </UL>
+ * The header contains fields used by the messaging system that describes the message's meta information,
+ * like QoS level, origin, destination, and so on, while the body contains the application data being transmitted.
+ * <p>
+ * As for the header, OMS defines two kinds types:  System Header and User Header,
+ * with respect to flexibility in vendor implementation and user usage.
+ * <ul>
+ * <li>
+ * System Header, OMS defines some standard attributes that represent the characteristics of the message.
+ * </li>
+ * <li>
+ * User Header, some OMS vendors may require enhanced extra attributes of the message
+ * or some users may want to clarify some customized attributes to draw the body.
+ * OMS provides the improved scalability for these scenarios.
+ * </li>
+ * </ul>
+ * The body contains the application data being transmitted,
+ * which is generally ignored by the messaging system and simply transmitted to its destination.
+ * <p>
+ * In BytesMessage, the body is just a byte array, may be compressed and uncompressed
+ * in the transmitting process by the messaging system.
+ * The application is responsible for explaining the concrete content and format of the message body,
+ * OMS is never aware of that.
  *
  * The body part is placed in the implementation classes of {@code Message}.
  *
@@ -42,80 +55,80 @@ package io.openmessaging;
  */
 public interface Message {
     /**
-     * Returns all the header fields of the {@code Message} object as a {@code KeyValue}.
+     * Returns all the system header fields of the {@code Message} object as a {@code KeyValue}.
      *
-     * @return the sysHeaders of a {@code Message}
+     * @return the system headers of a {@code Message}
      * @see BuiltinKeys
      */
     KeyValue sysHeaders();
 
     /**
-     * Returns all the built-in property fields of the {@code Message} object as a {@code KeyValue}.
+     * Returns all the customized user header fields of the {@code Message} object as a {@code KeyValue}.
      *
-     * @return the attributes of a {@code Message}
+     * @return the user headers of a {@code Message}
      */
     KeyValue userHeaders();
 
     /**
-     * Puts a {@code String}-{@code int} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code int} {@code KeyValue} entry to the system headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the system headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putSysHeaders(String key, int value);
 
     /**
-     * Puts a {@code String}-{@code long} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code long} {@code KeyValue} entry to the system headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the system headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putSysHeaders(String key, long value);
 
     /**
-     * Puts a {@code String}-{@code double} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code double} {@code KeyValue} entry to the system headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the system headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putSysHeaders(String key, double value);
 
     /**
-     * Puts a {@code String}-{@code String} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code String} {@code KeyValue} entry to the system headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the system headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putSysHeaders(String key, String value);
 
     /**
-     * Puts a {@code String}-{@code int} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code int} {@code KeyValue} entry to the user headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the user headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putUserHeaders(String key, int value);
 
     /**
-     * Puts a {@code String}-{@code long} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code long} {@code KeyValue} entry to the user headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the user headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putUserHeaders(String key, long value);
 
     /**
-     * Puts a {@code String}-{@code double} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code double} {@code KeyValue} entry to the user headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the user headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putUserHeaders(String key, double value);
 
     /**
-     * Puts a {@code String}-{@code String} {@code KeyValue} entry to he sysHeaders of a {@code Message}.
+     * Puts a {@code String}-{@code String} {@code KeyValue} entry to the user headers of a {@code Message}.
      *
-     * @param key the key to be placed into the sysHeaders
+     * @param key the key to be placed into the user headers
      * @param value the value corresponding to <tt>key</tt>
      */
     Message putUserHeaders(String key, String value);
@@ -125,12 +138,18 @@ public interface Message {
          * The {@code MessageId} header field contains a value that uniquely identifies
          * each message sent by a {@code Producer}.
          * <p>
-         * When a message is sent, MessageId is ignored.
-         * <p>
-         * When the send method returns it contains a producer-assigned value.
+         * When a message is sent, MessageId is assigned by the producer.
          */
         String MessageId = "MessageId";
 
+        /**
+         * The {@code Destination} header field contains the destination to which the message is being sent.
+         * <p>
+         * When a message is sent this value is set to the right {@code Queue}, then the message will be sent to
+         * the specified destination.
+         * <p>
+         * When a message is received, its destination is equivalent to the {@code Queue} where the message resides in.
+         */
         String Destination = "Destination";
 
 
@@ -140,8 +159,9 @@ public interface Message {
          * <p>
          * When a message is sent, BornTimestamp will be set with current timestamp as the born
          * timestamp of a message in client side, on return from the send method, the message's
-         * BornTimestamp header field contains this value. When a message is received its
-         * BornTimestamp header field contains this same value.
+         * BornTimestamp header field contains this value.
+         * <p>
+         * When a message is received its, BornTimestamp header field contains this same value.
          * <p>
          * This filed is a {@code long} value, measured in milliseconds.
          */
@@ -152,7 +172,8 @@ public interface Message {
          * <p>
          * When a message is sent, BornHost will be set with the local host info,
          * on return from the send method, the message's BornHost header field contains this value.
-         * When a message is received its BornHost header field contains this same value.
+         * <p>
+         * When a message is received, its BornHost header field contains this same value.
          */
         String BornHost = "BornHost";
 
@@ -232,12 +253,26 @@ public interface Message {
         /**
          * The {@code Reliability} header field contains the reliability level of a message.
          * A MOM server should guarantee the reliability level for a message.
+         * <p>
+         * OMS defines two modes of message delivery:
+         * <ul>
+         * <li>
+         * PERSISTENT, the persistent mode instructs the vendor should provide stable storage to
+         * ensure the message won't be lost.
+         * </li>
+         * <li>
+         * NON_PERSISTENT, this mode does not require that the message be logged to stable storage,
+         * the memory storage is enough to better performance and lower cost.
+         * </li>
+         * </ul>
          */
         String Reliability = "Reliability";
 
         /**
          * The {@code SearchKey} header field contains index search key of a message.
          * Clients can query similar messages by search key, and have a quick response.
+         * <p>
+         * This filed is a {@code String} value.
          */
         String SearchKey = "SearchKey";
 
