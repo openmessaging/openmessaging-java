fdbd753a5b99b451c4f31bc4a25e216a09cc4226
gaohaoxiang <gaohaoxiang@jd.com>
2019-03-01 15:32:52 +0800

    Fix KeyValue merge problem

--- openmessaging-api/src/main/java/io/openmessaging/KeyValue.java
+++ openmessaging-api/src/main/java/io/openmessaging/KeyValue.java
@@ -136,6 +136,17 @@ public interface KeyValue {
     int getInt(String key);
 
     /**
+     * Searches for the {@code int} property with the specified key in this {@code KeyValue} object. If the key is not
+     * found in this property list, the default value argument is returned.
+     *
+     * @param key the property key
+     * @param defaultValue a default value
+     * @return the value in this {@code KeyValue} object with the specified key value
+     * @see #put(String, int)
+     */
+    int getInt(String key, int defaultValue);
+
+    /**
      * Searches for the {@code long} property with the specified key in this {@code KeyValue} object. If the key is not
      * found in this property list, zero is returned.
      *
@@ -167,6 +178,17 @@ public interface KeyValue {
     double getDouble(String key);
 
     /**
+     * Searches for the {@code double} property with the specified key in this {@code KeyValue} object. If the key is
+     * not found in this property list, the default value argument is returned.
+     *
+     * @param key the property key
+     * @param defaultValue a default value
+     * @return the value in this {@code KeyValue} object with the specified key value
+     * @see #put(String, double)
+     */
+    double getDouble(String key, double defaultValue);
+
+    /**
      * Searches for the {@code String} property with the specified key in this {@code KeyValue} object. If the key is
      * not found in this property list, {@code null} is returned.
      *
@@ -175,6 +197,17 @@ public interface KeyValue {
      * @see #put(String, String)
      */
     String getString(String key);
+
+    /**
+     * Searches for the {@code String} property with the specified key in this {@code KeyValue} object. If the key is
+     * not found in this property list, the default value argument is returned.
+     *
+     * @param key the property key
+     * @param defaultValue a default value
+     * @return the value in this {@code KeyValue} object with the specified key value
+     * @see #put(String, String)
+     */
+    String getString(String key, String defaultValue);
 
     /**
      * Returns a {@link Set} view of the keys contained in this {@code KeyValue} object.
